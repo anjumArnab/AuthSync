097b7ceed11207c1faf7b89c0621850944229124
anjumArnab <anjumarnab050@gmail.com>
2025-03-24 17:14:17 +0600

    WidgetsFlutterBinding.ensureInitialized();

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+import 'package:authsync/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:authsync/screens/homepage.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
