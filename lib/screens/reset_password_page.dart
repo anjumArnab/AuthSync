aedb6d856ce6297c024c81bc79b3a32c05c78ce6
anjumArnab <anjumarnab050@gmail.com>
2025-03-25 20:52:51 +0600

    function:_changePassword()

--- lib/screens/reset_password_page.dart
+++ lib/screens/reset_password_page.dart
@@ -12,28 +12,23 @@ class ResetPasswordPage extends StatefulWidget {
 }
 
 class _ResetPasswordPageState extends State<ResetPasswordPage> {
+  final TextEditingController _oldPasswordController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
-  final TextEditingController _confirmPasswordController = TextEditingController();  
- bool _isLoading = false;
+  final TextEditingController _confirmPasswordController =
+      TextEditingController();
+  bool _isLoading = false;
   bool _isPasswordVisible = false;
 
   // Initialize the auth service
   final FirebaseAuthService _authService = FirebaseAuthService();
 
- Future<void> _changePassword() async {
+  Future<void> _changePassword() async {
+    final oldPassword = _oldPasswordController.text.trim();
     final newPassword = _passwordController.text.trim();
     final confirmPassword = _confirmPasswordController.text.trim();
 
-    if (newPassword.isEmpty || confirmPassword.isEmpty) {
-      showSnackBar(context, 'Please fill in both fields.');
-      return;
-    }
     if (newPassword != confirmPassword) {
       showSnackBar(context, 'Passwords do not match.');
-      return;
-    }
-    if (newPassword.length < 6) {
-      showSnackBar(context, 'Password must be at least 6 characters long.');
       return;
     }
 
@@ -41,85 +36,112 @@ class _ResetPasswordPageState extends State<ResetPasswordPage> {
       _isLoading = true;
     });
 
-    bool success = await _authService.changePassword(newPassword, context);
+    bool success =
+        await _authService.changePassword(oldPassword, newPassword, context);
 
     setState(() {
       _isLoading = false;
     });
 
     if (success) {
+      // Clear text fields
+      _oldPasswordController.clear();
+      _passwordController.clear();
+      _confirmPasswordController.clear();
+
       Navigator.pop(context); // Navigate back on success
     }
   }
-  
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("AuthSync"),
+      appBar: AppBar(
+        title: const Text("AuthSync"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            const Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                'Reset Password',
+                style: TextStyle(fontSize: 30),
+              ),
+            ),
+            const SizedBox(height: 15),
+            const Align(
+              alignment: Alignment.centerLeft,
+              child: Text('Old Password'),
+            ),
+            CustomTextField(
+                controller: _oldPasswordController,
+                hintText: 'Old Password',
+                obsecureText: !_isPasswordVisible,
+                suffixIcon: IconButton(
+                  icon: Icon(_isPasswordVisible
+                      ? Icons.visibility_outlined
+                      : Icons.visibility_off_outlined),
+                  onPressed: () {
+                    setState(() {
+                      _isPasswordVisible = !_isPasswordVisible;
+                    });
+                  },
+                ),
+                keyboardType: TextInputType.text),
+            const SizedBox(height: 15),
+            const Align(
+              alignment: Alignment.centerLeft,
+              child: Text('New Password'),
+            ),
+            CustomTextField(
+                controller: _passwordController,
+                hintText: 'New Password',
+                obsecureText: !_isPasswordVisible,
+                suffixIcon: IconButton(
+                  icon: Icon(_isPasswordVisible
+                      ? Icons.visibility_outlined
+                      : Icons.visibility_off_outlined),
+                  onPressed: () {
+                    setState(() {
+                      _isPasswordVisible = !_isPasswordVisible;
+                    });
+                  },
+                ),
+                keyboardType: TextInputType.text),
+            const SizedBox(height: 10),
+            const Align(
+              alignment: Alignment.centerLeft,
+              child: Text('Confirm Password'),
+            ),
+            CustomTextField(
+                controller: _confirmPasswordController,
+                hintText: 'Confirm Password',
+                obsecureText: !_isPasswordVisible,
+                suffixIcon: IconButton(
+                  icon: Icon(_isPasswordVisible
+                      ? Icons.visibility_outlined
+                      : Icons.visibility_off_outlined),
+                  onPressed: () {
+                    setState(() {
+                      _isPasswordVisible = !_isPasswordVisible;
+                    });
+                  },
+                ),
+                keyboardType: TextInputType.text),
+            const SizedBox(height: 10),
+            CustomButton(
+              text: _isLoading ? 'Resetting...' : 'Reset',
+              onPressed: _isLoading
+                  ? null
+                  : () async {
+                      await _changePassword();
+                    },
+            ),
+          ],
         ),
-        body: Padding(
-            padding: const EdgeInsets.all(16),
-            child: Column(
-              children: [
-                const Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    'Reset Password',
-                    style: TextStyle(fontSize: 30),
-                  ),
-                ),
-                const SizedBox(height: 15),
-                const Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text('New Password'),
-                ),
-                CustomTextField(
-                    controller: _passwordController,
-                    hintText: 'Password',
-                    obsecureText: !_isPasswordVisible,
-                    suffixIcon: IconButton(
-                      icon: Icon(_isPasswordVisible
-                          ? Icons.visibility_outlined
-                          : Icons.visibility_off_outlined),
-                      onPressed: () {
-                        setState(() {
-                          _isPasswordVisible = !_isPasswordVisible;
-                        });
-                      },
-                    ),
-                    keyboardType: TextInputType.text),
-                const SizedBox(height: 10),
-                const Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text('Confirm Password'),
-                ),
-                CustomTextField(
-                    controller: _confirmPasswordController,
-                    hintText: 'Password',
-                    obsecureText: !_isPasswordVisible,
-                    suffixIcon: IconButton(
-                      icon: Icon(_isPasswordVisible
-                          ? Icons.visibility_outlined
-                          : Icons.visibility_off_outlined),
-                      onPressed: () {
-                        setState(() {
-                          _isPasswordVisible = !_isPasswordVisible;
-                        });
-                      },
-                    ),
-                    keyboardType: TextInputType.text),
-                const SizedBox(height: 10),
-                CustomButton(
-                  text: _isLoading ? 'Resetting...' : 'Reset',
-                  onPressed: _isLoading
-                      ? null
-                      : () async {
-                          await _changePassword();
-                        },
-                ),
-              ],
-            )));
+      ),
+    );
   }
 }
